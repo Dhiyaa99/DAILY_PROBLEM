71945ab05e7ead337e74b3d8064fe3bc00f6218e
Dhiyaa99 <alnadabidhiyaa@gmail.com>
2025-04-20 10:02:34 +0400

    Update Hight-level.cs

--- Hight-level.cs
+++ Hight-level.cs
@@ -4,14 +4,14 @@ for (int i =1; i <= hieght; i++)
     int num = 1;
     //Console.WriteLine(num);
 
-    for (int j = 1; j <= i; j++)
+    for (int j = 1; j <= i; j++)  //i for external loop i is like r
     {
         Console.Write(num);
         num *= 2;
     }
     num /= 4;
 
-    for(int j =1; j<i; j++)
+    for(int j =1; j<i; j++)  // i is r   j is c   this is internal loop 
     {
         
         Console.Write(num);
